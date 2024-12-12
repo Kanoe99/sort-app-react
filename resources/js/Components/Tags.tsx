ebae9500a5755746ec79dff73143c2d2c33602c0
Kanoe99 <kanoe.s99@gmail.com>
2024-12-12 09:35:28 +0500

    tag ribbon: done

--- resources/js/Components/Tags.tsx
+++ resources/js/Components/Tags.tsx
@@ -1,10 +1,52 @@
+import { useState } from "react";
 import { Tag } from "@/types";
 
+const Button = ({
+  wrapperClasses,
+  arrowClasses,
+  onClick,
+}: {
+  wrapperClasses: string;
+  arrowClasses: string;
+  onClick: () => void;
+}) => {
+  return (
+    <div
+      className={`w-16 h-full from-black from-70% hover:from-80% transition duration-700 to-transparent to-100% top-0 absolute text-white px-4 flex items-center cursor-pointer ${wrapperClasses}`}
+      onClick={onClick}
+    >
+      <span
+        className={`border-l-4 border-b-4 block w-4 h-4 ${arrowClasses}`}
+      ></span>
+    </div>
+  );
+};
+
 const Tags = ({ tags }: { tags: Tag[] }) => {
+  const [tagsList, setTagsList] = useState(tags);
+
+  const shiftRight = () => {
+    const updatedTags = [...tagsList];
+    const firstTag = updatedTags.shift();
+    if (firstTag) {
+      updatedTags.push(firstTag);
+    }
+    setTagsList(updatedTags);
+  };
+
+  const shiftLeft = () => {
+    const updatedTags = [...tagsList];
+    const lastTag = updatedTags.pop();
+    if (lastTag) {
+      updatedTags.unshift(lastTag);
+    }
+    setTagsList(updatedTags);
+  };
+
   return (
     <div className="py-4 bg-black flex gap-2 overflow-hidden relative">
-      <div className="px-12 flex gap-2">
-        {tags.map((tag) => (
+      <div className="px-12 flex gap-2 transition-all duration-300">
+        {tagsList.map((tag) => (
           <a
             key={tag.id}
             className="block px-2 cursor-pointer text-white bg-[#303030] hover:bg-[#454545] rounded-md h-fit text-nowrap font-semibold transition-colors duration-300"
@@ -13,13 +55,18 @@ const Tags = ({ tags }: { tags: Tag[] }) => {
           </a>
         ))}
       </div>
-      <div className=" w-20 h-full top-0 bg-gradient-to-r from-black from-50% hover:from-60% transition duration-700 to-transparent to-100% absolute text-white px-4 flex items-center cursor-pointer">
-        <span className="border-l-4 border-b-4 block w-4 h-4 rotate-45 left-10"></span>
-      </div>
-      <div className="w-20 h-full top-0 right-0 bg-gradient-to-l from-black from-70% hover:from-80% transition duration-700 to-transparent to-100% absolute text-white px-4 flex items-center cursor-pointer">
-        <span className="border-l-4 border-b-4 block w-4 h-4 rotate-[225deg] right-10"></span>
-      </div>
+      <Button
+        wrapperClasses="bg-gradient-to-r left-0"
+        arrowClasses="rotate-45"
+        onClick={shiftRight}
+      />
+      <Button
+        wrapperClasses="bg-gradient-to-l justify-end right-0"
+        arrowClasses="rotate-[225deg]"
+        onClick={shiftLeft}
+      />
     </div>
   );
 };
+
 export { Tags };
